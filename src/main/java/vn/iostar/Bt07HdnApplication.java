57f27173c71082b07b7d507f1dbd95df290521d0
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-09-18 15:37:18 +0700

    first commit

---
+++ src/main/java/vn/iostar/Bt07HdnApplication.java
@@ -0,0 +1,13 @@
+package vn.iostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt07HdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt07HdnApplication.class, args);
+	}
+
+}
